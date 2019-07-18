b0e1aacf64421a371a52d56598020ed48d00aa08
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2019-07-18 19:04:29 +0300

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //selector - element <app-root></app-root>
+  // selector: '[app-root]', //selector - atribute <div app-root></div>
+  // selector: '.app-root', //selector - class <div class="app-root"></div>
   templateUrl: './app.component.html',
   /*styleUrls: ['./app.component.scss']*/
   styles: [`
     h1{
       color: blueviolet;
-    }
+     } 
     hr{
       color: #ddd;
       box-shadow: 0 2px 10px #888888;
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
 //   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  private serverId: number = 12345;
+  public serverName: string = '123s';
+  public allowNewServer: boolean = false;
+  public statusText: string = 'Server was not created';
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
+  }
+
+  getServerId () {
+    return this.serverId;
+  }
+
+  onCreateServer() {
+    this.allowNewServer = !this.allowNewServer;
+    this.allowNewServer ? this.statusText = 'Server created' : 'Server was not created';
   }
 
 }
