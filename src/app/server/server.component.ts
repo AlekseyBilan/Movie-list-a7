d011e9824a22c7d031030f01e6ab765cf938015c
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2019-07-24 09:51:32 +0300

    work with ngFor directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,39 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-//   styleUrls: ['./server.component.scss']
+  styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
-  private serverId: number = 12345;
-  private serverStatus: string = 'Offline';
-  public serverName: string = '';
-  public allowNewServer: boolean = false;
-  public serverCreated: boolean = false;
-  public statusText: string = 'Server was not created';
+  @Input() serverName: string;
+  public serverStatus: string = 'Offline';
+  // public serverName: string = '';
 
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? 'Offline' : 'Online';
+    console.log('serverName = ', this.serverName);
   }
 
   ngOnInit() {
   }
 
-  getServerId () {
-    return this.serverId;
-  }
-
-  onCreateServer() {
-    this.serverCreated = true;
-    this.statusText = 'Server created!, Name is '+ this.serverName;
-  }
-
-  changeServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
   getColor() {
-    return this.serverCreated ? 'green' : 'red';
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  private serverId: number = 12345;
+  private serverStatus: string = 'Offline';
+  public serverName: string = '';
+  public allowNewServer: boolean = true;
+  public serverCreated: boolean = false;
+  public statusText: string = 'Server was not created';
+  servers: Array<string> = ['TestServer 1', 'TestServer 2'];
 
-  constructor() { }
+  constructor() { 
+    setTimeout(()=> {
+      this.allowNewServer = false;
+    }, 2000)
+  }
 
   ngOnInit() {
+  }
+
+  getServerId () {
+    return this.serverId;
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.statusText = 'Server created!, Name is '+ this.serverName;
+  }
+
+  changeServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
